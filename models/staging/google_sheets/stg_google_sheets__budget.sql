4b258fd043401a9d76990279e617b261dbd3049d
Silvia Pasinato <silvia.pasinato@civica-soft.com>
2025-06-24 10:56:11 +0000

    recreando sources

---
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    schema='google_sheets'
+  )
+}}
+
+with
+
+    source as (select * from {{ source("google_sheets", "budget") }}),
+
+    renamed as (
+
+        select
+            _row,
+            quantity,
+            month,
+            product_id,
+            _fivetran_synced
+
+        from source
+
+    )
+
+select *
+from renamed
---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    schema='sql_server_dbo'
+  )
+}}
+
+
+with 
+
+source as (
+
+    select * from {{ source('sql_server_dbo', 'addresses') }}
+
+),
+
+renamed as (
+
+    select
+        address_id,
+        zipcode,
+        country,
+        address,
+        state,
+        _fivetran_deleted,
+        _fivetran_synced
+
+    from source
+
+)
+
+select * from renamed
